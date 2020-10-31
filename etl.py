a620ec74911a2af0ee2785091033b9ccb6dd8a47
Faris ALSaleem <faris.al.saleem15@gamil.com>
2020-10-31 13:14:42 +0300

    Add docstring

--- etl.py
+++ etl.py
@@ -4,18 +4,33 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Stages each table using the queries in `copy_table_queries` list.
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Insert transform data into each table using the queries in
+    `insert_table_queries` list.
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """Connects to Redshift database, extracts data from sparkify S3 bucket,
+    stages it redshift and transforms it into dimensionals
+
+    - Creates connects a database and initiate a session.
+    - Extracts user, song and metadata data from sparkify S3 bucket
+    - Transforms the staged data into a set of dimensional tables
+    - The connection is terminated in the end.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
